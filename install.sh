ff13b62afb2ed5dc4238c562c9359c86452e26e2
LittleStar-OuO <724804113@qq.com>
2022-11-20 15:04:30 +0800

    update.

--- install.sh
+++ install.sh
@@ -802,7 +802,7 @@ install_v2ray() {
 		cp -rf $(pwd)/* /etc/v2ray/233boy/v2ray
 	else
 		pushd /tmp
-		git clone https://github.com/LittleStar-OuO/v2ray.git -b "$_gitbranch" /etc/v2ray/233boy/v2ray --depth=1
+		git clone https://gitee.com/LittleStar_OuO/v2ray.git -b "$_gitbranch" /etc/v2ray/233boy/v2ray --depth=1
 		popd
 
 	fi
