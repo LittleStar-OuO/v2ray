1521cc73af545cd45e944710d8851d1490e4e305
LittleStar-OuO <724804113@qq.com>
2022-11-20 14:19:02 +0800

    update.

--- src/download-v2ray.sh
+++ src/download-v2ray.sh
@@ -18,7 +18,7 @@ _download_v2ray_file() {
 	[[ ! $v2ray_latest_ver ]] && _get_latest_version
 	v2ray_tmp_file="/tmp/v2ray.zip"
 	# v2ray_download_link="https://github.com/v2fly/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
-	v2ray_download_link="/script/v2ray-linux-64.zip"
+	v2ray_download_link="https://dokisu.com/v2ray-linux-64.zip"
 
 	if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
 		echo -e "
